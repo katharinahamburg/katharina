dbbaf0b310ccb639013b9e63c9367e791977d975
katharinahamburg <katharina@hinnrichs.de>
2023-07-18 11:48:55 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'katharina';
+export class AppComponent implements OnInit {
+images = ['moped.jpeg', 'yako.jpeg', 'wacken.jpeg'];
+headlines = ['Bring Blackbeauty to the next level!', 'Chill to the next level!', 'Bring ROCK to the next level!'];
+currentImage = 0;
+showImage = true;
+
+
+ngOnInit() {
+    this.updateImage();
+}
+
+
+updateImage() {
+
+    setInterval(() => {
+        this.currentImage++;
+        this.currentImage = this.currentImage % this.images.length;
+        this.showImage = false;
+
+        setTimeout(() => {
+          this.showImage = true;
+        }, 10);
+    }, 3000);
+}
+
+
 }
