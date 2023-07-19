94ad265045ba7a4a881ed5a28cee5aa2ad77042c
katharinahamburg <katharina@hinnrichs.de>
2023-07-19 15:31:35 +0200

    data protection added and ready to publish

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ImprintComponent } from './imprint/imprint.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
+import { DataProtectionComponent } from './data-protection/data-protection.component';
 
 
 
@@ -10,6 +11,7 @@ import { SlideshowComponent } from './slideshow/slideshow.component';
 
 
 const routes: Routes = [
+  { path: 'data-protection', component: DataProtectionComponent },
   { path: 'imprint', component: ImprintComponent },
   { path: '', component: SlideshowComponent }
 ];
